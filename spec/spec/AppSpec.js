8e36197f5b034a6dab5f7abfecc525ca5492736d
Jeff Dean <jeff@galvanize.it>
2015-03-02 00:10:11 -0700

    remove old spec

--- spec/spec/AppSpec.js
+++ spec/spec/AppSpec.js
@@ -1,13 +1,6 @@
 describe("Tic-Tac-Toe Board", function() {
 
   it("plays", function() {
-    var othello = new Othello();
-    expect(othello.board()).toEqual([
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ]);
-    expect(othello.turn).toEqual(2); // black
   });
 
 });
